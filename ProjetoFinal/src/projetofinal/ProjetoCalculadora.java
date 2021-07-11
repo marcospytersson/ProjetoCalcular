f72ef9631a3ecefcc76dfe104557d9791cf5fa52
MARCOS PYTERSSON <60246443+marcospytersson@users.noreply.github.com>
2021-07-11 16:42:27 -0300

    adicionando código - ProjetoCalculadora
    código - projeto calculadora

---
+++ ProjetoFinal/src/projetofinal/ProjetoCalculadora.java
@@ -0,0 +1,32 @@
+
+package projetofinal;
+
+
+public class ProjetoCalculadora {
+    
+    public double somar(double numero1, double numero2){
+     
+        int escolha;
+        double valorSoma = numero1 + numero2;
+        return valorSoma;
+        
+    }
+    
+    public double subtrair(double numero1, double numero2){
+        
+        double valorSubtrair = numero1 - numero2;
+        return valorSubtrair;
+    }
+     public double mutiplicar(double numero1, double numero2){
+        
+        double valorMutiplicar = numero1 * numero2;
+        return valorMutiplicar;
+    }
+     
+      public double dividir(double numero1, double numero2){
+        
+        double valorDividir = numero1 / numero2;
+        return valorDividir;
+    }    
+}
+
